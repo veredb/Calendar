de0f3819d2487aeea1d7aa86343cfa6227b7c5c7
vered bauer <veredbr2000@yahoo.com>
2012-09-29 11:03:40 -0500

    Last version of Cal2.test - prints the calendar

---
+++ test_cal2.rb
@@ -0,0 +1,10 @@
+require './cal2.rb'
+
+m = ARGV[0].to_i
+y = ARGV[1].to_i
+
+day_of_week_of_start(m, y)
+print print_month_year(m, y)
+print print_days_of_week_in_line
+print print_empty_line
+print the_days(m, y)
