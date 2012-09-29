6a64177df0ccf558f7d864d456e1f692414238bf
vered bauer <veredbr2000@yahoo.com>
2012-09-29 11:01:01 -0500

    last version of cal -

--- cal2.rb
+++ cal2.rb
@@ -12,43 +12,44 @@ def day_of_week_of_start(m, y)       # Zellers congruence
    end
 end
 
+  
+  
+def print_month_year(m, y)          # This function returns the title of month and year
   str = ""
-#  day_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  
-#  days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
-  
-#  months = {1=>"January", 2=>"February", 3=>"March", 4=>"April", 5=>"May", 6=>"June", 7=>"July", 8=>"August", 9=>"September", 10=>"October", 11=>"November", 12=>"December"}
-  
-  
-def print_month_year(m, y)
-  
   months = {1=>"January", 2=>"February", 3=>"March", 4=>"April", 5=>"May", 6=>"June", 7=>"July", 8=>"August", 9=>"September", 10=>"October", 11=>"November", 12=>"December"}
-  months.each do |num,mon|     #prints the desired month and year
+  months.each do |num,mon|     
      if num == m
-#        str +=  "\t" + "\t" + months[num] + " "
-        return months[num]
-     end
+        str +=  "\t" + "\t" + months[num] + " "
+       end
   end
   str +=  y.to_s + "\n"
+  str
 end
 
 
-def print_days_of_week_in_line  
+def print_days_of_week_in_line             # This function returns the line of the days Su Mo Tu.....
+  str = ""
   days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
   days.each do |a|     
-#     str += a + "\t"
+     str += a + "\t"
   end
+  str
 end
   
-  for x in 1..7 do     # prints the empty line under Su, Mo, T.....
+def print_empty_line    # This function returns an empty line under Su Mo Tu.....
+  str = ""
+  for x in 1..7 do     
     if x % 7 == 0
-       str += "\t\n"
+       str += "\n"
     else 
-       str += "\t"
+       str +=  "\t"
     end
   end
+  str
+end
 
-def the_days(m, y)
+def the_days(m, y)            # This function returns the starting day of week and the number of days in a month(checks for leap year)
+
   day_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   str =""
   dow = day_of_week_of_start(m, y) - 1  # number of spaces before the starting day of month
@@ -57,7 +58,7 @@ def the_days(m, y)
      str += "\t"
   end
   feb_in_leap_year = 0
-  if (m == 2 && y%4 == 0 && y%100 == 0 && y%400 == 0)
+  if (m == 2 && y%4 == 0 && y%100 == 0 && y%400 == 0)      #checks for leap year
     feb_in_leap_year = 1
   end
   for days in 1..(day_in_month[m] + feb_in_leap_year) do
@@ -67,8 +68,6 @@ def the_days(m, y)
       str +=  days.to_s + "\t"
     end
   end
-  return days
   str += "\n"
   str
 end
-#calendar(ARGV[0].to_i,ARGV[1].to_i)
